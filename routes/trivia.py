7005881c9c9430b48132477cfd5514dd49b23a77
Luvay Moiz <luvay@Luvays-MacBook-Air.local>
2025-09-06 11:16:24 +0800

    answers latest

--- routes/trivia.py
+++ routes/trivia.py
@@ -8,14 +8,14 @@ logger = logging.getLogger(__name__)
 @app.route("/trivia", methods=["GET"])
 def trivia():
     answers = [
-    1,                  # Q1: "Trivia!"
-    3,                  # Q2: "Ticketing Agent"
+    2,                  # Q1: "Trivia!"
+    1,                  # Q2: "Ticketing Agent"
     2,                  # Q3: "Blankety Blanks"
     2,                  # Q4: "Princess Diaries"
     3,                  # Q5: "MST Calculation"
     4,                  # Q6: "Universal Bureau of Surveillance" (Amy Winehouse)
     1,                  # Q7: "Operation Safeguard"
-    [1, 2, 3, 4, 5],    # Q8: "Capture The Flag" (all are anagrams)
+    5,    # Q8: "Capture The Flag" (all are anagrams)
     4                   # Q9: "Filler 1"
   ]
     logging.info("answers : %s", answers)
