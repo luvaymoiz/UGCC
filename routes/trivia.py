085b967282a773ff4cd467b21fff08b2e9dda1ba
Luvay Moiz <luvay@Luvays-MacBook-Air.local>
2025-09-06 10:24:47 +0800

    trivia

---
+++ routes/trivia.py
@@ -0,0 +1,22 @@
+import logging
+from flask import jsonify
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+@app.route("/trivia", methods=["GET"])
+def trivia():
+    answers = [
+        1,               
+        3,       
+        2,       
+        2,
+        3,
+        4,
+        1,
+        [1, 2, 3, 4, 5],
+        4 
+    ]
+    return jsonify(answers)
+
+
